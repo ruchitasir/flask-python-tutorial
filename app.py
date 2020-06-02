55a7111c99f63eecfe28c4dcbb094f0ee57fcfa2
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-06-02 16:14:22 -0700

    commented app.py file

--- app.py
+++ app.py
@@ -22,28 +22,25 @@ db = client.flask_database
 def home():
   return render_template('index.html')
 
+# loop route renders all loops from database
 @app.route("/loops")
 def loops_home():
     retrieved_loops= db.loops.find()
- 
     return render_template('loops.html',retrieved_loops= retrieved_loops)  
 
+# operator route renders all operators from database and also renders a form for the uesr to add new operator
+# and it displays the new operator into the existing list on the page
 @app.route("/operators", methods = ['POST', 'GET'])  
 def operators_home():  
     retrived_operators = db.operators.find()
     if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        symbol = request.form['symbol']
-        example = request.form['example']
-        uses = request.form['uses']
-        print('req.form',request.form)
+      # print('req.form',request.form)
         oper={
-            'name':name,
-            'description':description,
-            'symbol': symbol,
-            'example': example,
-            'uses':uses
+            'name':request.form['name'],
+            'description': request.form['description'],
+            'symbol': request.form['symbol'],
+            'example': request.form['example'],
+            'uses':request.form['uses']
         }
         db.operators.insert_one(oper).inserted_id
     return render_template('operators.html',retrived_operators= retrived_operators)          
